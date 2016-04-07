5768bd59f09e1a55264dff07581e980e2871425d
Valgeir <valgeirb@gmail.com>
2016-04-07 21:01:18 +0000

    Simplified structure of app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,1 @@
-export class App {
-  configureRouter(config, router) {
-    config.title = 'Magic Mirror';
-    config.map([{
-      route: ['', 'magic-mirror'],
-      name: 'magic-mirror',
-      moduleId: 'magic-mirror',
-      nav: true,
-      title: 'Magic Mirror'
-    }]);
-
-    this.router = router;
-  }
-}
+export class App {}
